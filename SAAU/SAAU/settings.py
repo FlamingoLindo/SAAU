21e5f49ccb07ebb7eb1df09a64683b98e2e60c35
FlamingoLindo <101421364+FlamingoLindo@users.noreply.github.com>
2025-05-21 09:53:07 -0300

    feat: add CORS support and update requirements for django-cors-headers

--- SAAU/SAAU/settings.py
+++ SAAU/SAAU/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -45,7 +46,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'rest_framework_simplejwt.token_blacklist',
     'sensitive_info',
-    'drf_yasg'
+    'drf_yasg',
 ]
 
 REST_FRAMEWORK = {
@@ -96,6 +97,7 @@ SIMPLE_JWT = {
 AUTH_USER_MODEL = 'saau_api.CustomUser'
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -103,6 +105,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "https://projetofinalfabiano.netlify.app/",
 ]
 
 ROOT_URLCONF = 'SAAU.urls'
